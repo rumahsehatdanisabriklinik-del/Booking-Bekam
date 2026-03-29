f892e6dd1b2e7e475038010903c4e1b609a428e3
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 12:14:31 +0700

    perbaikan slot booking

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbyk4Es4dlZgmQZ2ZTiYGx854EI0lWnfRxqySnRI7kv7sGRb81kEa_1OgglfJqv0KSyCwA/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbygOQ4P8PXFS7gIzD-9eLL0755ibgh7x1e1PZCwJwUmRT0F2SDBvcqIHlecT3yRcxddDg/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
