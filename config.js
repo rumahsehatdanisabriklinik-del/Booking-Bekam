ebb7370ed58e375dbda0be48d691b562bcf3cc5a
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 12:09:30 +0700

    Refactor: Modularisasi Backend & Implementasi Laporan Otomatis

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbz0BYMX6X9Py-eoehfAAr5XyA9VRwZPvF1_C5crAwE4dZMBWEaNIZ5Qd_249PaunP_oMA/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbyk4Es4dlZgmQZ2ZTiYGx854EI0lWnfRxqySnRI7kv7sGRb81kEa_1OgglfJqv0KSyCwA/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
