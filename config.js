2df3a67f9cfb7fd4ace92dfad5785a2f1929ac50
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 15:56:29 +0700

    otorisasi perijinan

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbyQ0PICFGT56NS3eO-bjTP78FK70AfX9VPd1mTPfGeNWtHlmGr5YfTkFhky7yQVdoBnqg/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbyOCU1mU-zmkIXI2aTXpzmKYm0PiipyJ4_mgGX5cLGGTUgDRZPm9Kia7mlgJy-ZXnt0rw/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
