b8382c962dfc359a146d9fc3612966497d5f4b8f
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 09:32:01 +0700

    fix(dashboard): mendobrak local scope browser pada config.js agar GAS_URL terbaca global

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyV0ZouZpt4wUCbr0BzUk7ELcwMOggnWnv1R-QU4hoU7cay6oMmizXQVRX7xtwFnOjZ/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbyV0ZouZpt4wUCbr0BzUk7ELcwMOggnWnv1R-QU4hoU7cay6oMmizXQVRX7xtwFnOjZ/exec";
+window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
