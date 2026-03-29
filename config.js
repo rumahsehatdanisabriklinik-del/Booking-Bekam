e14d7c42874dc80079f5d6b6d2b7a9ff64e1eb50
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 11:53:44 +0700

    fix(calendar): perbaiki scroll date strip bebas 2 arah + tambah tombol panah navigasi

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbw-eTZln0nUihDHsG6yT3sUqmXmnd9e5GoISYOtBZLYpoOE-hbxsX5wJvEG3rogQ2j9jg/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbxtjVsDCeQVgxoUjwc2Hmm2154M0NyTVzB18mcy2P_0vCu0tAKioI0zR6pzfdmJE8FA_A/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
