28f7a885fab7be64dcb27784a9ff3e331e226e59
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 16:11:56 +0700

    perbaikan notif email dan google calender

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbyOCU1mU-zmkIXI2aTXpzmKYm0PiipyJ4_mgGX5cLGGTUgDRZPm9Kia7mlgJy-ZXnt0rw/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbwkDpj7h6qdLPI5MeHGB7rfjGfFmuxAWgss7UxJrzvG3mHWkbpM4K4urhSy6aFeEbJs5g/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
