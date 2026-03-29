626918cb0ece7ad6280b3e9e345d4abdfa124d95
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 09:48:33 +0700

    feat(dashboard): fix input no di lacak booking

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbyV0ZouZpt4wUCbr0BzUk7ELcwMOggnWnv1R-QU4hoU7cay6oMmizXQVRX7xtwFnOjZ/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbwHTIMnCn17muj-eKg_lw5V1Lynqr9o65kx3YH9ISQTkEqWxy1VlnrxwgY-1KWFOx57JQ/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
