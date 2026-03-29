a83a517d63ab117e8bbe8f50bcc668d1c71880c3
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-03-29 12:22:42 +0700

    sinkronisasi header

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbygOQ4P8PXFS7gIzD-9eLL0755ibgh7x1e1PZCwJwUmRT0F2SDBvcqIHlecT3yRcxddDg/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbzGFgnuPwwOpR8d56xh7vVaKntPz8O4bogoQ9hlNinDIIGd5haF27bS6D2MOdjMF8_KVw/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
