772867699a2644b80ecd2bfe180538410cc4178e
rumahsehatdanisabriklinik-del <rumahsehatdanisabriklinik@gmail.com>
2026-04-03 21:38:24 +0700

    setting cms

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Ganti URL Deployment Google Apps Script (GAS_URL) Anda di file ini HANYA SEKALI.
 // Perubahan akan otomatis tertaut dan menular ke seluruh aplikasi (Index, Admin, Status).
 
-var GAS_URL = "https://script.google.com/macros/s/AKfycbwkDpj7h6qdLPI5MeHGB7rfjGfFmuxAWgss7UxJrzvG3mHWkbpM4K4urhSy6aFeEbJs5g/exec";
+var GAS_URL = "https://script.google.com/macros/s/AKfycbzzhVTWZokLJZQ0mduVO6yMDgqp7BWTp8MykV4bP6BdXHYDU8Mu44jBASEwzuOj6e_iZw/exec";
 window.GAS_URL = GAS_URL; // Kepastian mutlak variabel global
